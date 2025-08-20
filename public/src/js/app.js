8dd90c79c25ec710e2b627c4afd337b06735da31
Aleksnadar <aleksandar.dimovski09@gmail.com>
2025-08-20 15:48:10 +0200

    countinued with the course

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,5 @@
+var deferredPrompt;
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
@@ -5,3 +7,23 @@ if ('serviceWorker' in navigator) {
         console.log('Service worker registered!');
         });
 }
+
+window.addEventListener('beforeinstallprompt', function(event) {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+});
+
+var promise = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+    resolve('This is executed once the timer is done!');
+    //console.log('This is executed once the timer is done!');
+}, 3000);
+});
+
+promise.then(function() {
+
+});
+
+console.log('This is executed right after setTimeout!');
