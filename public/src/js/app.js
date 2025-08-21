77c1a737eb4edee0d3c20064be10e5beba85d6d9
Aleksnadar <aleksandar.dimovski09@gmail.com>
2025-08-21 14:05:26 +0200

    countinued with fetch and promises

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -22,8 +22,55 @@ var promise = new Promise(function(resolve, reject) {
 }, 3000);
 });
 
-promise.then(function() {
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://httpbin.org/ip');
+xhr.responseType = 'json';
+xhr.onload = function() {
+    console.log(xhr.response);
+};
+xhr.onerror = function() {
+    console.log('Error!');
+};
+xhr.send();
 
+fetch('https://httpbin.org/ip')
+    .then(function(response) {
+    console.log(response);
+    return response.json();
+})
+    .then(function(data) {
+    console.log(data);
+})
+    .catch(function(err) {
+    console.log(err);
+    });
+
+
+    fetch('https://httpbin.org/post', {
+        method: 'POST', 
+        headers:{
+            'Content-Type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify({ message: 'Does this work?' })
+    })
+    .then(function(response) {
+    console.log(response);
+    return response.json();
+})
+    .then(function(data) {
+    console.log(data);
+})
+    .catch(function(err) {
+    console.log(err);
+    });
+
+promise.then(function(text) {
+    return text;
+}).then(function(newText) {
+    console.log(newText);
+}).catch(function(err) {
+    console.log(err.code, err.message);
 });
 
 console.log('This is executed right after setTimeout!');
