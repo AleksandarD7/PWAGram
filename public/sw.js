a035200d0a10852945770ad20208710d57984e80
Aleksnadar <aleksandar.dimovski09@gmail.com>
2025-09-12 17:14:59 +0200

    added to firebase

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,17 @@
 
+var CACHE_STATIC_NAME = 'static-v8';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static')
+    caches.open(CACHE_STATIC_NAME)
       .then(function(cache) {
         console.log('[Service Worker] Precaching App Shell');
         cache.addAll([
           '/',
           '/index.html',
+          '/offline.html',
           '/src/js/app.js',
           '/src/js/feed.js',
           '/src/js/promise.js',
@@ -26,26 +30,43 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
+  event.respondWith(
     caches.match(event.request)
-        .then(function(response) {
-            if (response) {
-                return response;
-            } else {
-                return fetch(event.request)
-                  .then(function(res) {
-                    return caches.open('dynaic')
-                      .then(function(cache) {
-                        cache.put(event.request.url, res.clone());
-                        return res;
-                      });
-                  })
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function(err) {
+              return caches.open(CACHE_STATIC_NAME)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
 
-            }
-        })
-    );
+            });
+        }
+      })
+  );
 });
